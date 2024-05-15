69178432faca9808567a0ea168e89fc9e1272c17
hm-4 <hm.iisc.iitb@gmail.com>
2024-05-15 13:00:08 +0530

    Time: 97 ms (76.24%) | Memory: 18.7 MB (16.70%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -12,20 +12,15 @@ class Solution:
 
             if left_height < right_height: 
                 left_ptr += 1
-                new_left_height = height[left_ptr]
-                water_body = left_peak - new_left_height
-                left_height = new_left_height
-                if left_height > left_peak:
-                    left_peak = left_height
+                left_height = height[left_ptr]
+                water_body = left_peak - left_height
+                left_peak = max(left_peak, left_height)
             else:
                 right_ptr -= 1
-                new_right_height = height[right_ptr]
-                water_body = right_peak - new_right_height
-                right_height = new_right_height
-                if right_height > right_peak:
-                    right_peak = right_height
+                right_height = height[right_ptr]
+                water_body = right_peak - right_height
+                right_peak = max(right_peak, right_height)
             
-            print(left_ptr, right_ptr, water_body)
             water += water_body if water_body > 0 else 0
         return water
 
