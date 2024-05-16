ef4d2234a2b2f487fcb9a920b824c82bc720dfc2
hm-4 <hm.iisc.iitb@gmail.com>
2024-05-16 14:53:03 +0530

    Time: 745 ms (58.75%) | Memory: 27.3 MB (90.82%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,25 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         i = 0
+#         profit = 0
+#         smaller_val = prices[0]
+#         for i in range(len(prices)):
+#             cur_val = prices[i]
+#             profit = max(profit, cur_val - smaller_val) # this updates the upside.
+#             if smaller_val > cur_val: # this update the down side.
+#                 smaller_val = cur_val
+#             i += 1
+#         return profit
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i = 0
+        buy_price = prices[0]
         profit = 0
-        smaller_val = prices[0]
-        for i in range(len(prices)):
-            cur_val = prices[i]
-            profit = max(profit, cur_val - smaller_val) # this updates the upside.
-            if smaller_val > cur_val: # this update the down side.
-                smaller_val = cur_val
-            i += 1
+
+        for p in prices[1:]:
+            if buy_price > p:
+                buy_price = p
+            
+            profit = max(profit, p - buy_price)
+        
         return profit
