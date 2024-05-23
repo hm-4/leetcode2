64444d52f99f8c8b580decf966b5c630263da464
hm-4 <hm.iisc.iitb@gmail.com>
2024-05-23 15:50:43 +0530

    Time: 653 ms (83.85%) | Memory: 37.8 MB (66.63%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,6 +1,26 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        """
+        if we sort the array then the reaching times has the following charectaristics
+        target: 12, target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+        --------------------
+        reaching times: a for 10, b for 11 ....
+          0123456789abcd
+        10: *
 
+        08: *
+
+
+        05:       *
+
+        03:   *
+
+
+        00:            *
+
+        from top every peak is one fleet.!
+        for example fleet is at 10, 05, and 00. therefore three fleets for this example
+        """
         fleet = 0
         # caluclate times
         individual_times = []
