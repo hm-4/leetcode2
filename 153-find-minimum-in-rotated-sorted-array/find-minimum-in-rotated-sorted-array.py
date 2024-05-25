695c1385757d73e6c821374544d59063709fc0dd
hm-4 <hm.iisc.iitb@gmail.com>
2024-05-25 21:30:49 +0530

    Time: 36 ms (90.94%) | Memory: 16.8 MB (97.64%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -2,27 +2,26 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         left_ptr = 0
         right_ptr = len(nums) - 1
-        left_val = nums[left_ptr]
-        right_val = nums[right_ptr]
 
-        if left_val < right_val: 
-            return left_val
+
+        if nums[left_ptr] < nums[right_ptr]: 
+            return nums[left_ptr]
 
         while left_ptr < right_ptr:
             mid_ptr = (left_ptr + right_ptr) // 2
-            mid_val = nums[mid_ptr]
+            # mid_val = nums[mid_ptr]
             # print(left_ptr, right_ptr, mid_ptr, left_val, right_val, mid_val)
-            if mid_val >= left_val and left_val > right_val:
+            if nums[mid_ptr] >= nums[left_ptr] and nums[left_ptr] > nums[right_ptr]:
                 left_ptr = mid_ptr + 1
                 # mid_ptr is always in the list since
                 # since interdivision will result in this phenomenon.
                 # (1+2) // 2 = 1 and 1+1 = 2, two is already valid index.
-                left_val = nums[left_ptr]
+                # left_val = nums[left_ptr]
             else:
                 right_ptr = mid_ptr  
                 # should not be right_ptr=mid_ptr-1 because 
                 # the the mid_val can be the minimum val 
-                right_val = nums[right_ptr]
+                # right_val = nums[right_ptr]
             
-        return left_val
+        return nums[left_ptr]
         
