d762e1ef15bfb3faaa2c063a22f46c5d9bcf451e
hm-4 <hm.iisc.iitb@gmail.com>
2024-05-25 21:34:57 +0530

    Time: 48 ms (19.99%) | Memory: 16.9 MB (72.16%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -9,19 +9,25 @@ class Solution:
 
         while left_ptr < right_ptr:
             mid_ptr = (left_ptr + right_ptr) // 2
-            # mid_val = nums[mid_ptr]
             # print(left_ptr, right_ptr, mid_ptr, left_val, right_val, mid_val)
             if nums[mid_ptr] >= nums[left_ptr] and nums[left_ptr] > nums[right_ptr]:
+                """
+                it should be greater than or equal since for a case like [2, 1]
+                and nums[left_ptr] > nums[right_ptr]: statement should be there
+                when the pointer are on the right side of the array which is in 
+                increasing order, we have to distinguish that from the left side
+                case where we the array may not be in increasing order.
+                """
                 left_ptr = mid_ptr + 1
                 # mid_ptr is always in the list since
                 # since interdivision will result in this phenomenon.
                 # (1+2) // 2 = 1 and 1+1 = 2, two is already valid index.
-                # left_val = nums[left_ptr]
+
             else:
                 right_ptr = mid_ptr  
                 # should not be right_ptr=mid_ptr-1 because 
                 # the the mid_val can be the minimum val 
-                # right_val = nums[right_ptr]
+
             
         return nums[left_ptr]
         
